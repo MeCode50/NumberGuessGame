ae8cfa364a0e5202199c4f3208ce5d87d1167386
*muna* <*victorycollins31@gmail.com*>
2022-08-28 10:17:01 +0100

    Else statement  if isdigit isnt a number

--- NumberGame.py
+++ NumberGame.py
@@ -6,6 +6,9 @@ if top_range.isdigit():
     if top_range <=0:
         print('please type a number greater than 0 ')
         quit()
+else:
+    print("What you entered is not a number, type a number next time")
+    quit()
 
 num = random.randrange(101)
 
