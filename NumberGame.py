079eccf1e4e29f144014db9bede0a2ae93dc1455
*muna* <*victorycollins31@gmail.com*>
2022-08-28 10:09:48 +0100

    if statment to know if top range is digit

--- NumberGame.py
+++ NumberGame.py
@@ -1,6 +1,11 @@
 import random
 
-top_range = input("Input a Number ")
+top_range = input("Enter a number: ")
+if top_range.isdigit():
+    top_range = int(top_range)
+    if top_range <=0:
+        print('please type a number greater than 0 ')
+        quit()
 
 num = random.randrange(101)
-print(num)
+
