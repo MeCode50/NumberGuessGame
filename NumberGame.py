41e5b7442fc77c919528c1eaf53131da2e264441
*muna* <*victorycollins31@gmail.com*>
2022-08-28 11:01:37 +0100

    statemnt to check if user gues=random num

--- NumberGame.py
+++ NumberGame.py
@@ -10,16 +10,20 @@ else:
     print("What you entered is not a number, type a number next time")
     quit()
 
-num = random.randrange(top_range)
-print(num)
+random_num = random.randint(0, top_range)
 
 while True:
-    user_guess = input("Guess a number: ")
-    if user_guess.isdigit():
-        user_guess = int(user_guess)
-    else:
-        print('type in a number next time')
-        continue
+     user_guess = input("Guess a number: ")
+     if user_guess.isdigit():
+         user_guess = int(user_guess)
+     else:
+         print('type in a number next time')
+         continue
+     if user_guess == random_num:
+         print('correct')
+         break
+     else:
+         print('try again')
 
-    
+
 
